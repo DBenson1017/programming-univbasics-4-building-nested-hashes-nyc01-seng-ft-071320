79753aec9503adfefa2a0cc399a6673fb4dabac4
Devin Benson <devinbenson@gmail.com>
2020-06-26 21:00:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,9 @@ end
 
 def monopoly_with_second_tier
  monopoly={
-    railroads: {
-      pieces: "4"
-    }
+    railroads: {}
   }
+  monopoly[:railroads][:pieces]="4"
 end
 
  # When you start writing the implementation for this method, copy the Hash
